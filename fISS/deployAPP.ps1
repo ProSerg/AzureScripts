755353a65847ec12831a0c1f2e3c1181ac7bd4d5
Markin Sergey <smarkin@vrconcept.net>
2017-09-30 17:31:49 +0400

    update

--- fISS/deployAPP.ps1
+++ fISS/deployAPP.ps1
@@ -1,6 +1,8 @@
 git clone https://github.com/pallets/flask-website.git ./Website/flask-website
 pip install Flask
 Set-Location ./Website/flask-website
-$env:FLASK_APP=./Website/flask-website/run.py
+$env:FLASK_APP=C:\Website\flask-website\run.py
+Get-Location .
+Get-ChildItem .
 flask run
 
