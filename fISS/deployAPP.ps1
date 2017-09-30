471195f0bd699eef5938330d8eedd02b07b283bd
Markin Sergey <smarkin@vrconcept.net>
2017-09-30 17:47:40 +0400

    update

--- fISS/deployAPP.ps1
+++ fISS/deployAPP.ps1
@@ -4,6 +4,7 @@ pip install Flask-OpenID
 pip install Flask-SQLAlchemy
 pip install whoosh
 pip install python-creole
+pip install Creoleparser
 Set-Location ./Website/flask-website
 $env:FLASK_APP=C:\Website\flask-website\run.py
 flask run
