9a29e1b4f8650b26565f9ee59d631f2a03414bc4
Markin Sergey <smarkin@vrconcept.net>
2017-09-30 17:52:07 +0400

    update

--- fISS/deployAPP.ps1
+++ fISS/deployAPP.ps1
@@ -1,11 +1,6 @@
 git clone https://github.com/pallets/flask-website.git ./Website/flask-website
-pip install Flask
-pip install Flask-OpenID
-pip install Flask-SQLAlchemy
-pip install whoosh
-pip install python-creole
-pip install Creoleparser
 Set-Location ./Website/flask-website
+pip install -r requirements.txt
 $env:FLASK_APP=C:\Website\flask-website\run.py
 flask run
 
