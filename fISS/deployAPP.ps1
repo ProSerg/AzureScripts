c5637703ed73e7d48c9375b73ba9d307deeec22c
Markin Sergey <smarkin@vrconcept.net>
2017-09-30 17:44:19 +0400

    update

--- fISS/deployAPP.ps1
+++ fISS/deployAPP.ps1
@@ -3,9 +3,8 @@ pip install Flask
 pip install Flask-OpenID
 pip install Flask-SQLAlchemy
 pip install whoosh
+pip install python-creole
 Set-Location ./Website/flask-website
-Get-Location .
-Get-ChildItem .
 $env:FLASK_APP=C:\Website\flask-website\run.py
 flask run
 
